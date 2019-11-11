04cdbfcf94c36cdd24c95fdc3184f70baa39fe66
David May <me@davidmay.de>
2019-11-12 00:15:20 +0100

    dummy tweet exercise

--- jsx-exercises/src/index.js
+++ jsx-exercises/src/index.js
@@ -2,7 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 function MyThing() {
-
+    return(
+        <div className="book">
+            <div className="title">
+                The Title
+            </div>
+            <div className="author">
+                The Author
+            </div>
+            <ul className="stats">
+                <li className="rating">
+                    5 stars
+                </li>
+                <li className='isbn'> 12-345678-910
+                </li>
+            </ul>
+        </div>
+    )
 }
 
 ReactDOM.render(
