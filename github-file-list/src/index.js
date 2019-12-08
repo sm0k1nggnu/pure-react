6e43914ccfaa02b75d0851306adea4e51c31eab8
David May <me@davidmay.de>
2019-12-08 21:32:14 +0100

    more components

--- github-file-list/src/index.js
+++ github-file-list/src/index.js
@@ -1,19 +1,65 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
+import './index.css';
 
 const FileList = ({ files }) => (
     <table className="file-list">
         <tbody>
             {files.map(file => (
-                <tr className="file-list-item" key={file.id}>
-                <td className="file-name">{file.name}</td> </tr>
+                <FileListItem key={ file.id } file={ file }/>
             ))}
         </tbody>
     </table>
 );
 FileList.propTypes = {
     files: PropTypes.array
+};
+
+function FileName({ file }) {
+    return (
+        <>
+            <FileIcon file={ file } />
+            <td className="file-name">{file.name}</td>
+        </>
+    );
+}
+FileName.propTypes = {
+file: PropTypes.object.isRequired
+};
+
+function FileIcon({ file }) {
+    let icon = 'fa-file-tex-o';
+    if(file.type === 'folder') {
+        icon = 'fa-folder'
+    }
+    return (
+        <td className="file-icon">
+            <i className={`fa ${icon}`} />
+        </td>
+    );
+}
+FileIcon.propTypes = {
+    file: PropTypes.object.isRequired
+};
+
+const CommitMessage = ({ commit }) => (
+    <td className="commit-message">
+        {commit.message}
+    </td>
+);
+CommitMessage.propTypes = {
+commit: PropTypes.object.isRequired };
+
+const FileListItem = ({ file }) => (
+    <tr className="file-list-item" key={file.id}>
+        <FileName file={ file } />
+        <CommitMessage commit={file.latestCommit}/>
+        <td className="file-time">{file.updated_at}</td>
+    </tr>
+);
+FileListItem.propTypes = {
+    file: PropTypes.object.isRequired
 };
 
 const testFiles = [
