5a74b325ec60c030afc8d6415ee62ce1ab35db7e
David May <me@davidmay.de>
2019-12-23 18:30:38 +0100

    incorporate moment.js

--- github-file-list/src/index.js
+++ github-file-list/src/index.js
@@ -1,87 +1,103 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
+import Time from './time';
 import './index.css';
 
 const FileList = ({ files }) => (
-    <table className="file-list">
-        <tbody>
-            {files.map(file => (
-                <FileListItem key={ file.id } file={ file }/>
-            ))}
-        </tbody>
-    </table>
+  <table className="file-list">
+    <tbody>
+      {files.map(file =>
+        /* we use FileListItem here */
+        <FileListItem key={file.id} file={file}/>
+      )}
+    </tbody>
+  </table>
 );
 FileList.propTypes = {
-    files: PropTypes.array
+  files: PropTypes.array
 };
 
-function FileName({ file }) {
-    return (
-        <>
-            <FileIcon file={ file } />
-            <td className="file-name">{file.name}</td>
-        </>
-    );
-}
-FileName.propTypes = {
-file: PropTypes.object.isRequired
-};
-
-function FileIcon({ file }) {
-    let icon = 'fa-file-tex-o';
-    if(file.type === 'folder') {
-        icon = 'fa-folder'
-    }
-    return (
-        <td className="file-icon">
-            <i className={`fa ${icon}`} />
-        </td>
-    );
-}
-FileIcon.propTypes = {
-    file: PropTypes.object.isRequired
+const FileListItem = ({ file }) => (
+	<tr className="file-list-item">
+		<td className="file-icon">
+			<FileIcon file={file}/>
+		</td>
+		<td className="file-name">
+			<FileName file={file}/>
+		</td>
+		<td className="commit-message">
+			<CommitMessage commit={file.latestCommit} />
+		</td>
+		<td className="age">
+			<Time time={file.updated_at}/>
+		</td>
+	</tr>
+);
+FileListItem.propTypes = {
+  file: PropTypes.object.isRequired
 };
 
 const CommitMessage = ({ commit }) => (
-    <td className="commit-message">
-        {commit.message}
-    </td>
+	<span>{commit.message}</span>
 );
 CommitMessage.propTypes = {
-commit: PropTypes.object.isRequired };
+  commit: PropTypes.object.isRequired
+};
 
-const FileListItem = ({ file }) => (
-    <tr className="file-list-item" key={file.id}>
-        <FileName file={ file } />
-        <CommitMessage commit={file.latestCommit}/>
-        <td className="file-time">{file.updated_at}</td>
-    </tr>
-);
-FileListItem.propTypes = {
-    file: PropTypes.object.isRequired
+function FileIcon({ file }) {
+  let icon = 'fa-file-text-o';
+  if(file.type === 'folder') {
+    icon = 'fa-folder';
+  }
+
+  return (
+		<i className={`fa ${icon}`}/>
+  );
+}
+FileIcon.propTypes = {
+  file: PropTypes.object.isRequired
+};
+
+function FileName({ file }) {
+  return (
+    <span>{file.name}</span>
+  );
+}
+FileName.propTypes = {
+  file: PropTypes.object.isRequired
 };
 
 const testFiles = [
-    {
-        id: 1,
-        name: 'src',
-        type: 'folder',
-        updated_at: "2016-07-11 21:24:00", latestCommit: {
-        message: 'Initial commit' }
-    }, {
-        id: 2,
-        name: 'tests',
-        type: 'folder',
-        updated_at: "2016-07-11 21:24:00", latestCommit: {
-        message: 'Initial commit' }
-    }, {
-        id: 3,
-        name: 'README',
-        type: 'file',
-        updated_at: "2016-07-18 21:24:00", latestCommit: {
-        message: 'Added a readme' }
-    },
-]
+  {
+    id: 1,
+    name: 'src',
+    type: 'folder',
+    updated_at: "2016-07-11 21:24:00",
+    latestCommit: {
+      message: 'Initial commit'
+    }
+  },
+  {
+    id: 2,
+    name: 'tests',
+    type: 'folder',
+    updated_at: "2016-07-11 21:24:00",
+    latestCommit: {
+      message: 'Initial commit'
+    }
+  },
+  {
+    id: 3,
+    name: 'README',
+    type: 'file',
+    updated_at: "2016-07-18 21:24:00",
+    latestCommit: {
+      message: 'Added a readme'
+    }
+  },
+];
 
-ReactDOM.render(<FileList files={ testFiles }/>, document.querySelector('#root'));
+ReactDOM.render(<FileList files={testFiles}/>, document.querySelector('#root'));
+
+export { FileList, testFiles };  // for testing
