c6b0088b857218ee8e3ec36c461dfcca5ef3faff
David May <me@davidmay.de>
2020-01-02 00:18:12 +0100

    state chapter + click dummy

---
+++ state/src/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Child({ onAction }) { return (
+  <button onClick={onAction}> Click Me!
+  </button>
+  ); }
+
+class CountingParent extends React.Component {
+  // constructor called when component created
+  constructor(props) {
+    super(props)
+    //set state
+    this.state = {
+      actionCount: 0
+    };
+    this.handleAction = this.handleAction.bind(this)
+  }
+
+  handleAction(action) {
+    console.log('Child says', action)
+    this.setState({
+      actionCount: this.state.actionCount + 1
+    });
+  }
+
+  render() {
+    return(
+      <div>
+        <Child onAction={this.handleAction}/>
+        <p>Clicked {this.state.actionCount} times</p>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<CountingParent/>, document.querySelector('#root'));
+
+// export { FileList, testFiles };  // for testing
